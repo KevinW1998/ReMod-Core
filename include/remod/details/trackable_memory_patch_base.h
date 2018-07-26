d0a8553a858cacedc652d1f87f97b9f5ea3dfc40
Kevsoft <kevin.waldock@gmail.com>
2018-07-26 21:48:03 +0200

    trackable_memory_patch_base can be only constructed as base class

--- include/remod/details/trackable_memory_patch_base.h
+++ include/remod/details/trackable_memory_patch_base.h
@@ -15,10 +15,9 @@ namespace remod
 		void set_data(std::uintptr_t patch_ptr, std::vector<std::uint8_t>&& data);
 		trackable_memory_patch_base() = default;
 
-	public:
 		trackable_memory_patch_base(std::uintptr_t patch_ptr, const std::vector<std::uint8_t>& data);
 		trackable_memory_patch_base(std::uintptr_t patch_ptr, std::vector<std::uint8_t>&& data);
-
+	public:
 		trackable_memory_patch_base(const trackable_memory_patch_base& other) = delete;
 		trackable_memory_patch_base(trackable_memory_patch_base&& other) = delete;
 		trackable_memory_patch_base& operator=(const trackable_memory_patch_base& other) = delete;
@@ -26,7 +25,6 @@ namespace remod
 
 		virtual ~trackable_memory_patch_base();
 
-		// TODO: Use trackable_patch
 		virtual void on_patch();
 		virtual void on_unpatch();
 	};
