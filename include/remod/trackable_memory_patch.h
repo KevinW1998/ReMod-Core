bf77d570992ea8811067d3c53ddb3ff9c5b4fb51
Kevsoft <kevin.waldock@gmail.com>
2018-07-26 22:04:47 +0200

    Add WIP class trackable_memory_patch

---
+++ include/remod/trackable_memory_patch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "details/trackable_memory_patch_base.h"
+#include <vector>
+
+namespace remod
+{
+	class REMOD_CORE_EXPORT trackable_memory_patch : public trackable_memory_patch_base
+	{
+	public:
+		// TODO: How to handle pointer?
+	};
+}
+
+
